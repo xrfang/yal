4474a572ea53f04ff76a9bd1b29112b8d3b1be31
xrfang <xrfang@gmail.com>
2023-04-27 10:33:12 +0800

    removed debugging info

--- bind.go
+++ bind.go
@@ -181,10 +181,6 @@ func Log(item LogItem) {
 	if opt.Trace {
 		st := trace(false)
 		if len(st) > 0 {
-			fmt.Printf("tracing:\n")
-			for _, s := range st {
-				fmt.Println(">", s)
-			}
 			item.Attr["~src~"] = st[0]
 		}
 	}
